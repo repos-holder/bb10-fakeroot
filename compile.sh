8bd826d3cb18c4830bcefeeed94abc82b472dffc
A. V. Lukyanov <artem.falcon@gmail.com>
2014-10-04 07:37:40 +0400

    easy debugging

--- compile.sh
+++ compile.sh
@@ -7,5 +7,5 @@ PBHOSTARCH=arm-unknown-nto-qnx8.0.0eabi
 PBBUILDARCH=x86_64-apple-darwin
 PBTARGETARCH=arm-unknown-nto-qnx8.0.0eabi 
  
-./configure --host=$PBHOSTARCH --build=$PBBUILDARCH --target=$PBTARGETARCH --with-ipc=tcp --prefix=$tmp_pwd/../dest --program-transform-name=
+./configure --host=$PBHOSTARCH --build=$PBBUILDARCH --target=$PBTARGETARCH --with-ipc=tcp --prefix=$tmp_pwd/dest --program-transform-name=
 make install
