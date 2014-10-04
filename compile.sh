2e8f8ca69b4723cb93db5cffc620528e4744fce8
A. V. Lukyanov <artem.falcon@gmail.com>
2014-10-04 07:14:01 +0400

    compile script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ 
+tmp_pwd=`pwd`
+cd /Applications/user-installed/QNX/Momentics.app && source bbndk-* && cd $tmp_pwd
+
+PBHOSTARCH=arm-unknown-nto-qnx8.0.0eabi
+PBBUILDARCH=x86_64-apple-darwin
+PBTARGETARCH=arm-unknown-nto-qnx8.0.0eabi 
+ 
+./configure --host=$PBHOSTARCH --build=$PBBUILDARCH --target=$PBTARGETARCH --with-ipc=tcp --prefix=$tmp_pwd/../dest
+make install
