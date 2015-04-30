4e56c74843a764da5328d803381a2876ca013b0d
A. V. Lukyanov <dukzcry@users.noreply.github.com>
2015-04-30 10:09:58 +0300

    add profile and adjust paths

---
+++ dest/env.sh
@@ -0,0 +1,12 @@
+export QNX_HOST=/accounts/1000/shared/documents/fakeroot
+
+export PATH="$QNX_HOST/bin:$PATH"
+# emulate devuser
+export FAKEROOTUID=100
+export FAKEROOTGID=100
+export FAKEROOTEUID=100
+export FAKEROOTEGID=100
+export FAKEROOTSUID=100
+export FAKEROOTSGID=100
+export FAKEROOTFUID=100
+export FAKEROOTFGID=100
